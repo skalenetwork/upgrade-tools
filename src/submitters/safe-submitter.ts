9ad02483ee225b161b3bbb3c9faf01984506c22b
Vadim Yavorsky <yavrsky@gmail.com>
2023-04-03 21:33:00 +0300

    Refactor safe-submitter

--- src/submitters/safe-submitter.ts
+++ src/submitters/safe-submitter.ts
@@ -1,7 +1,6 @@
-import { BigNumber, UnsignedTransaction } from "ethers";
-import { ethers, network } from "hardhat";
-import { createMultiSendTransaction, sendSafeTransaction } from "../gnosis-safe";
-import { encodeTransaction } from "../multiSend";
+import { UnsignedTransaction } from "ethers";
+import { ethers } from "hardhat";
+import { createMultiSendTransaction } from "../gnosis-safe";
 import { Submitter } from "./submitter";
 
 export class SafeSubmitter extends Submitter {
@@ -15,28 +14,9 @@ export class SafeSubmitter extends Submitter {
     }
 
     async submit(transactions: UnsignedTransaction[]): Promise<void> {
-        const safeTransactions: string[] = [];
-        for (const transaction of transactions) {
-            safeTransactions.push(encodeTransaction(
-                0,
-                transaction.to ? transaction.to : ethers.constants.AddressZero,
-                transaction.value ? BigNumber.from(transaction.value) : 0,
-                transaction.data ? transaction.data.toString() : "0x"
-            ))
-        }
-
-        let privateKey = (network.config.accounts as string[])[0];
-        if (network.config.accounts === "remote") {
-            // Don't have an information about private key
-            // Use random one because we most probable run tests
-            privateKey = ethers.Wallet.createRandom().privateKey;
-        }
-
         if (!this.chainId) {
             this.chainId = (await ethers.provider.getNetwork()).chainId;
         }
-
-        const safeTx = await createMultiSendTransaction(ethers, this.safeAddress, privateKey, safeTransactions, this.chainId);
-        await sendSafeTransaction(this.safeAddress, this.chainId, safeTx);
+        await createMultiSendTransaction(this.safeAddress, transactions);
     }
 }
