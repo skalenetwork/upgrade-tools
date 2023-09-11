2e28e7d2b1e21ce979a7d06947fb959ed34bb2d4
Dmytro Stebaiev <dimastebaev@gmail.com>
2023-09-11 17:22:25 +0300

    Add searching of version file

--- src/version.ts
+++ src/version.ts
@@ -1,9 +1,24 @@
 import { exec as asyncExec } from "child_process";
-import { promises as fs } from "fs";
+import { dirname } from "path";
+import { promises as fs, existsSync } from "fs";
 import util from "util";
 
 
 const exec = util.promisify(asyncExec);
+
+async function getVersionFilename(folder?: string) {
+    if (folder === undefined) {
+        return getVersionFilename(process.cwd());
+    }
+    const path = `${folder}/VERSION`;
+    if (existsSync(path)) {
+        return path;
+    }
+    if (folder === '/') {
+        throw Error("Can't find version file");
+    }
+    return getVersionFilename(dirname(folder));
+}
 
 export const getVersion = async () => {
     if (process.env.VERSION) {
@@ -13,9 +28,6 @@ export const getVersion = async () => {
         const tag = (await exec("git describe --tags")).stdout.trim();
         return tag;
     } catch {
-        return (await fs.readFile(
-            "VERSION",
-            "utf-8"
-        )).trim();
+        return (await fs.readFile(await getVersionFilename(), "utf-8")).trim();
     }
 };
