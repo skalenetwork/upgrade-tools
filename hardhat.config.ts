39429cc149c8f6a117bbc30a97c82636cc87daf6
Vadim Yavorsky <yavrsky@gmail.com>
2024-01-12 16:45:40 +0200

    Fix build

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,7 +7,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: '0.8.13',
+                version: '0.8.11',
                 settings: {
                     optimizer: {
                         enabled: true,
