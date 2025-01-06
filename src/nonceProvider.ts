eacc816181508da9c6eeab50f6021904809ac6d0
Vadim Yavorsky <yavrsky@gmail.com>
2025-01-06 19:33:54 +0000

    Fix nonce reservation logic in NonceProvider

--- src/nonceProvider.ts
+++ src/nonceProvider.ts
@@ -14,7 +14,7 @@ export class NonceProvider {
     }
 
     reserveNonce () {
-        if (!this.releasedNonces) {
+        if (this.releasedNonces.length === 0) {
             const nonce = this.currentNonce;
             this.currentNonce += 1;
             return nonce;
