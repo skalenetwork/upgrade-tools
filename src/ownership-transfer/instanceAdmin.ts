d9a5dd44a299428055602edc63abafc5b5136913
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2025-12-09 18:55:01 +0000

    code-style adjustments

--- src/ownership-transfer/instanceAdmin.ts
+++ src/ownership-transfer/instanceAdmin.ts
@@ -62,30 +62,6 @@ export class InstanceAdmin {
         this.processOptions(options);
     }
 
-    private async initialize(): Promise<void> {
-        await this.loadContractMetadataAndCreateTransactions();
-        await this.confirmData();
-    }
-
-    private async processGrantStepIfRequired(): Promise<void> {
-        if (this.ownershipGrantingRequired()) {
-            await this.submitGrantOwnershipTransactions();
-            // TODO: Create step to wait for txs to be confirmed in case of Multisig submitter
-            if (this.revokeRoles) {
-                await this.createRequiredTransactions();
-            }
-            await this.confirmData();
-        }
-    }
-
-    private async processRevokeStepIfRequired(): Promise<void> {
-        if (this.revokeRoles && this.ownershipRevokingRequired() === true) {
-            await this.submitRevokeRolesTransactions();
-            // TODO: Create step to wait for txs to be confirmed in case of Multisig submitter
-            await this.createRequiredTransactions();
-        }
-    }
-
     public async executeOwnershipTransfer(): Promise<void> {
         // Confirm data found in initialization
         await this.initialize();
@@ -100,6 +76,28 @@ export class InstanceAdmin {
         }
         else{
             console.log(chalk.green("Ownership transfer process completed. - Nothing more to do."));
+        }
+    }
+
+        private async initialize(): Promise<void> {
+        await this.loadContractMetadataAndCreateTransactions();
+        await this.confirmData();
+    }
+
+    private async processGrantStepIfRequired(): Promise<void> {
+        if (this.ownershipGrantingRequired()) {
+            await this.submitGrantOwnershipTransactions();
+            if (this.revokeRoles) {
+                await this.createRequiredTransactions();
+            }
+            await this.confirmData();
+        }
+    }
+
+    private async processRevokeStepIfRequired(): Promise<void> {
+        if (this.revokeRoles && this.ownershipRevokingRequired() === true) {
+            await this.submitRevokeRolesTransactions();
+            await this.createRequiredTransactions();
         }
     }
 
@@ -122,6 +120,12 @@ export class InstanceAdmin {
             throw new Error("Transaction submission aborted by user.");
         }
         await this.submitter.submit(txsToSubmit.map(tx => tx.transaction));
+        if (this.submitter instanceof SafeSubmitter) {
+            await this.promptConfirmation(
+                "Please confirm ONLY AFTER the Safe transactions have been executed on-chain.\n" +
+                "Do you want to proceed?"
+            );
+        }
     }
 
     private async submitGrantOwnershipTransactions(): Promise<void> {
@@ -130,7 +134,6 @@ export class InstanceAdmin {
             txsToSubmit.push(...contract.getGrantOwnershipTransactions());
             contract.clearGrantTransactions();
         }
-
         // Required for ProxyAdmins shared among multiple contracts
         const txsToSubmitFiltered = removeDuplicateTransactions(txsToSubmit);
         console.log(chalk.green(`Submitting ${txsToSubmitFiltered.length} unique grant ownership transactions...`));
@@ -156,12 +159,7 @@ export class InstanceAdmin {
         /* eslint-disable no-await-in-loop */
         // Do not parallelize to avoid rate limit issues which CAN produce wrong outputs
         for (const contractName of this.contractNames) {
-            /*
-             * We use getContractAddress to not depend on downloaded ABI artifacts
-             * all ABIs are checked locally
-             */
             const address = await this.instance.getContractAddress(contractName);
-
             if (!this.contractMetadata.has(address)) {
                 const pattern = await detectPattern(address);
                 const details: ContractMetadataDetails = {
@@ -179,24 +177,18 @@ export class InstanceAdmin {
 
     private async confirmData(): Promise<void> {
         this.displayFindings();
-
         const userConfirmed = await this.promptConfirmation();
-
         if (!userConfirmed) {
             this.handleUserRejection();
         }
-
         console.log(chalk.green("\nUser confirmed. Proceeding...\n"));
     }
 
     private processOptions(options: InstanceAdminOptions): void {
-        // If true, does not allow to send transactions to blockchain
-
         this.bytes32RolesToCheck = (options.rolesToCheck ?? []).map(role => ({
             identifier: ethers.id(role),
             name: role
         }));
-        // Always have DEFAULT_ADMIN_ROLE at the end!
         this.bytes32RolesToCheck.push({identifier: ethers.ZeroHash, name: "DEFAULT_ADMIN_ROLE"});
         if (!this.readonly && this.newOwner === ethers.ZeroAddress) {
             throw new Error("New owner address must be provided in options when in write mode.");
@@ -207,7 +199,6 @@ export class InstanceAdmin {
             }
             return {identifier: role, name: `Role ${role}`};
         });
-        // Always have ADMIN_ROLE at the end!
         this.managerRolesToCheck.push({identifier: ZERO, name: "ADMIN_ROLE"});
     }
 
@@ -262,7 +253,6 @@ export class InstanceAdmin {
     private displayFindings(): void {
         console.log(chalk.cyan("\n=== Ownership Status Results ===\n"));
         const groupedByPattern = this.groupMetadataByPattern();
-
         for (const [pattern, contracts] of Object.entries(groupedByPattern)) {
             if (contracts.length) {
                 this.displayPatternGroup(pattern, contracts);
@@ -283,11 +273,9 @@ export class InstanceAdmin {
                 acc[pattern] = [];
                 return acc;
             }, {} as Record<Pattern, ContractAdmin[]>);
-
         for (const metadata of this.contractMetadata.values()) {
             grouped[metadata.pattern].push(metadata);
         }
-
         return grouped;
     }
 
